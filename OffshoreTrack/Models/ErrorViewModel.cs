35743ab16e363dc1baabe3411164278a16dd7fc6
Kezia Campos <kezia.campos@icloud.com>
2023-06-28 12:54:32 -0300

    somente site

--- OffshoreTrack/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OffshoreTrack.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
